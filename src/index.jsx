0678d1f4b2b4a5ffa334e22a1ad9a080d2127660
Wallace Nascimento Pereira <wallace.pereira@semantix.com.br>
2021-04-25 08:46:25 -0300

    feat: add theming to aplication

---
+++ src/index.jsx
@@ -0,0 +1,56 @@
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import App from 'App';
+
+const rootElement = document.querySelector('#root');
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#d3fbff',
+      main: '#3BA3BA',
+      dark: '#297a82',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      light: '#ba703b',
+      main: '#824f29',
+      dark: '#5b361d',
+      contrastText: '#ffffff',
+    },
+  },
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Hind',
+      'Quicksand',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    overrides: {
+      MuiCssBaseline: {
+        '@global': {
+          'font-family': 'Quicksand',
+        },
+      },
+    },
+  },
+});
+function Main() {
+  return (
+    <StrictMode>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </StrictMode>
+  );
+}
+
+ReactDOM.render(<Main />, rootElement);
